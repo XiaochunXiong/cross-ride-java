a4071f018f29bc0fc24a8382b1a1da66d666a82a
Jerry Xiong <jerry.xc.xiong@gmail.com>
2018-09-24 16:21:34 -0400

    init project

---
+++ src/main/java/com/crossover/techtrial/CrossRideApplication.java
@@ -0,0 +1,9 @@
+package com.crossover.techtrial;
+
+public class CrossRideApplication {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
